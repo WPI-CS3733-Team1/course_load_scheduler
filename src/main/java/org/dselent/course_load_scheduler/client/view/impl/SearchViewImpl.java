3e2a28dddea33c49f0817743949f3a507f50a5c5
Alex <aralvarez@wpi.edu>
2018-02-15 00:33:10 -0500

    added search view pathway -alex

---
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/SearchViewImpl.java
@@ -0,0 +1,92 @@
+package org.dselent.course_load_scheduler.client.view.impl;
+
+import org.dselent.course_load_scheduler.client.presenter.SearchPresenter;
+import org.dselent.course_load_scheduler.client.view.SearchView;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.RadioButton;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
+
+public class SearchViewImpl extends BaseViewImpl<SearchPresenter> implements SearchView {
+
+	private static SearchViewImplUiBinder uiBinder = GWT.create(SearchViewImplUiBinder.class);
+
+	interface SearchViewImplUiBinder extends UiBinder<Widget, SearchViewImpl> {
+	}
+	
+	@UiField
+	RadioButton users_button;
+	
+	@UiField
+	RadioButton courses_button;
+	
+	@UiField
+	TextBox dept_box;
+	
+	@UiField
+	AbsolutePanel search_panel;
+	
+	public SearchViewImpl() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	@Override
+	public void setPresenter(SearchPresenter presenter) {
+
+		this.presenter = presenter;
+	}
+
+	@Override
+	public Widget getWidgetContainer() {
+		
+		return this;
+	}
+
+	@Override
+	public HasWidgets getViewRootPanel() {
+		
+		return search_panel;
+	}
+
+	@Override
+	public RadioButton getUsersRadioButton() {
+		
+		return users_button;
+	}
+
+	@Override
+	public void setUsersRadioButton(RadioButton usersRadioButton) {
+		
+		this.users_button = usersRadioButton;
+	}
+
+	@Override
+	public RadioButton getCoursesRadioButton() {
+		
+		return courses_button;
+	}
+
+	@Override
+	public void setCoursesRadioButton(RadioButton coursesRadioButton) {
+		
+		this.courses_button = coursesRadioButton;
+	}
+
+	@Override
+	public TextBox getDeptIDTextBox() {
+		
+		return dept_box;
+	}
+
+	@Override
+	public void setDeptIDTextBox(TextBox nameTextBox) {
+		
+		this.dept_box = nameTextBox;
+	}
+
+}
