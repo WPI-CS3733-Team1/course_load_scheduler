f6986bffb380bbe00ade1b9fa944d5ba923dd9ea
noah parker <nparker@wpi.edu>
2018-02-19 21:58:54 -0500

    Noah - Overrode all Handler functions into EventHandlerAdapter. Can now be selectively implemented in PresenterImpls

--- src/main/java/org/dselent/course_load_scheduler/client/event_handler/EventHandlerAdapter.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event_handler/EventHandlerAdapter.java
@@ -1,8 +1,16 @@
 package org.dselent.course_load_scheduler.client.event_handler;
 
 import org.dselent.course_load_scheduler.client.event.InvalidCreateCourseEvent;
+import org.dselent.course_load_scheduler.client.event.InvalidCreateSectionEvent;
 import org.dselent.course_load_scheduler.client.event.InvalidLoginEvent;
+import org.dselent.course_load_scheduler.client.event.OpenCreateAdminEvent;
+import org.dselent.course_load_scheduler.client.event.OpenCreateCourseEvent;
+import org.dselent.course_load_scheduler.client.event.OpenInboxEvent;
+import org.dselent.course_load_scheduler.client.event.OpenScheduleEvent;
+import org.dselent.course_load_scheduler.client.event.OpenSearchEvent;
 import org.dselent.course_load_scheduler.client.event.ReceiveLoginEvent;
+import org.dselent.course_load_scheduler.client.event.SendCreateCourseEvent;
+import org.dselent.course_load_scheduler.client.event.SendCreateSectionEvent;
 import org.dselent.course_load_scheduler.client.event.SendLoginEvent;
 
 /**
@@ -16,9 +24,12 @@ import org.dselent.course_load_scheduler.client.event.SendLoginEvent;
  * 
  */
 public abstract class EventHandlerAdapter
-implements InvalidLoginEventHandler, SendLoginEventHandler, ReceiveLoginEventHandler,
-			InvalidCreateCourseEventHandler
+implements InvalidLoginEventHandler, SendLoginEventHandler, OpenCreateAdminEventHandler, OpenCreateCourseEventHandler, ReceiveLoginEventHandler,
+			InvalidCreateCourseEventHandler, InvalidCreateSectionEventHandler, OpenInboxEventHandler, OpenScheduleEventHandler,
+			OpenSearchEventHandler, SendCreateSectionEventHandler, SendCreateCourseEventHandler
 {
+	//Noah - Still need to write ExecuteSearchEventHandler/Action/Event pathway
+	
 	@Override
 	public void onInvalidLogin(InvalidLoginEvent evt) {}
 	
@@ -26,10 +37,33 @@ implements InvalidLoginEventHandler, SendLoginEventHandler, ReceiveLoginEventHan
 	public void onSendLogin(SendLoginEvent evt) {}
 	
 	@Override
+	public void onOpenCreateAdmin(OpenCreateAdminEvent evt) {}
+	
+	@Override
+	public void onOpenCreateCourse(OpenCreateCourseEvent evt) {}
+	
+	@Override
 	public void onInvalidCreateCourse(InvalidCreateCourseEvent evt) {}
 	
 	@Override
 	public void onReceiveLogin(ReceiveLoginEvent evt) {}
 	
-	//add overridden empty methods
+	@Override
+	public void onInvalidCreateSection(InvalidCreateSectionEvent evt) {}
+
+	@Override
+	public void onOpenInbox(OpenInboxEvent evt) {}
+	
+	@Override
+	public void onOpenSchedule(OpenScheduleEvent evt) {}
+	
+	@Override
+	public void onOpenSearch(OpenSearchEvent evt) {}
+	
+	@Override
+	public void onSendCreateCourse(SendCreateCourseEvent evt) {}
+	
+	@Override
+	public void onSendCreateSection(SendCreateSectionEvent evt) {}
+	
 }
