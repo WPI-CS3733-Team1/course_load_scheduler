ac01bbc281f69de6842babf148048aa634f70c65
bwagner <bpwagner15@wpi.edu>
2018-02-21 14:28:50 -0500

    Added events to eventHandlerAdapter

--- src/main/java/org/dselent/course_load_scheduler/client/event_handler/EventHandlerAdapter.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event_handler/EventHandlerAdapter.java
@@ -3,6 +3,8 @@ package org.dselent.course_load_scheduler.client.event_handler;
 import org.dselent.course_load_scheduler.client.event.InvalidCreateCourseEvent;
 import org.dselent.course_load_scheduler.client.event.InvalidCreateSectionEvent;
 import org.dselent.course_load_scheduler.client.event.InvalidLoginEvent;
+import org.dselent.course_load_scheduler.client.event.InvalidOpenCreateAdminEvent;
+import org.dselent.course_load_scheduler.client.event.InvalidOpenInboxEvent;
 import org.dselent.course_load_scheduler.client.event.OpenCreateAdminEvent;
 import org.dselent.course_load_scheduler.client.event.OpenCreateCourseEvent;
 import org.dselent.course_load_scheduler.client.event.OpenCreateSectionEvent;
@@ -10,9 +12,13 @@ import org.dselent.course_load_scheduler.client.event.OpenInboxEvent;
 import org.dselent.course_load_scheduler.client.event.OpenScheduleEvent;
 import org.dselent.course_load_scheduler.client.event.OpenSearchEvent;
 import org.dselent.course_load_scheduler.client.event.ReceiveLoginEvent;
+import org.dselent.course_load_scheduler.client.event.ReceiveOpenCreateAdminEvent;
+import org.dselent.course_load_scheduler.client.event.ReceiveOpenInboxEvent;
 import org.dselent.course_load_scheduler.client.event.SendCreateCourseEvent;
 import org.dselent.course_load_scheduler.client.event.SendCreateSectionEvent;
 import org.dselent.course_load_scheduler.client.event.SendLoginEvent;
+import org.dselent.course_load_scheduler.client.event.SendOpenCreateAdminEvent;
+import org.dselent.course_load_scheduler.client.event.SendOpenInboxEvent;
 
 /**
  * Adapter class for convenience
@@ -25,9 +31,10 @@ import org.dselent.course_load_scheduler.client.event.SendLoginEvent;
  * 
  */
 public abstract class EventHandlerAdapter
-implements InvalidLoginEventHandler, SendLoginEventHandler, OpenCreateAdminEventHandler, OpenCreateCourseEventHandler, ReceiveLoginEventHandler,
-			InvalidCreateCourseEventHandler, InvalidCreateSectionEventHandler, OpenInboxEventHandler, OpenScheduleEventHandler,
-			OpenSearchEventHandler, SendCreateSectionEventHandler, SendCreateCourseEventHandler, OpenCreateSectionEventHandler
+implements InvalidLoginEventHandler, SendLoginEventHandler, OpenCreateAdminEventHandler, SendOpenCreateAdminEventHandler, InvalidOpenCreateAdminEventHandler,
+			OpenCreateCourseEventHandler, ReceiveLoginEventHandler,InvalidCreateCourseEventHandler, InvalidCreateSectionEventHandler, OpenInboxEventHandler, 
+			OpenScheduleEventHandler,OpenSearchEventHandler, SendCreateSectionEventHandler, SendCreateCourseEventHandler, OpenCreateSectionEventHandler,
+			SendOpenInboxEventHandler, InvalidOpenInboxEventHandler, ReceiveOpenInboxEventHandler, ReceiveOpenCreateAdminEventHandler
 {
 	//Noah - Still need to write ExecuteSearchEventHandler/Action/Event pathway
 	
@@ -42,6 +49,24 @@ implements InvalidLoginEventHandler, SendLoginEventHandler, OpenCreateAdminEvent
 	
 	@Override
 	public void onOpenCreateCourse(OpenCreateCourseEvent evt) {}
+	
+	@Override
+	public void onSendOpenCreateAdmin(SendOpenCreateAdminEvent evt) {}
+	
+	@Override
+	public void onInvalidOpenCreateAdmin(InvalidOpenCreateAdminEvent evt) {}
+	
+	@Override
+	public void onSendOpenInbox(SendOpenInboxEvent evt) {}
+	
+	@Override
+	public void onInvalidOpenInbox(InvalidOpenInboxEvent evt) {}
+	
+	@Override
+	public void onReceiveOpenInbox(ReceiveOpenInboxEvent evt) {}
+	
+	@Override
+	public void onReceiveOpenCreateAdmin(ReceiveOpenCreateAdminEvent evt) {}
 	
 	@Override
 	public void onInvalidCreateCourse(InvalidCreateCourseEvent evt) {}
