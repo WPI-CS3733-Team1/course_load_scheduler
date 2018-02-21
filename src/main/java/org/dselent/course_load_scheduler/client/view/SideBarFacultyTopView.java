94eae4e7acad3320742d10bb3ee686d37867e913
Harsh Rana <hr23232323@gmail.com>
2018-02-20 20:38:38 -0500

    Harsh: Sidebar pathway being fixed to be concise

---
+++ src/main/java/org/dselent/course_load_scheduler/client/view/SideBarFacultyTopView.java
@@ -0,0 +1,8 @@
+package org.dselent.course_load_scheduler.client.view;
+
+import org.dselent.course_load_scheduler.client.presenter.SideBarPresenter;
+
+public interface SideBarFacultyTopView extends BaseView<SideBarPresenter>{
+	
+
+}
